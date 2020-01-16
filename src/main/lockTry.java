d6268d70d7bfc3762e3b37cac7e656d3d48c4cf2
Slava <firepoint.pro@gmail.com>
2020-01-16 19:39:02 +0300

    Reentrant lock check

---
+++ src/main/lockTry.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class lockTry {
+    public static void main(String[] args) {
+        Resource resource = new Resource();
+        resource.setLocalVar(3);
+        firstThread thread1 = new firstThread();
+        firstThread thread2 = new firstThread();
+        thread1.setResource(resource);
+        thread2.setResource(resource);
+        thread1.start();
+        thread2.start();
+        System.out.println(resource.getLocalVar());
+    }
+
+    static class Resource {
+        private int localVar;
+        Lock lock = new ReentrantLock();
+
+        public int getLocalVar() {
+            return localVar;
+        }
+
+        public void setLocalVar(int localVar) {
+            this.localVar = localVar;
+        }
+
+        public void changeVar(){
+            lock.lock();
+            localVar++;
+            lock.unlock();
+        }
+
+    }
+
+    static class firstThread extends Thread {
+        private Resource resource;
+
+        public Resource getResource() {
+            return resource;
+        }
+
+        public void setResource(Resource resource) {
+            this.resource = resource;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " Im Started");
+            resource.changeVar();
+            System.out.println(resource.getLocalVar());
+        }
+    }
+
+
+}
